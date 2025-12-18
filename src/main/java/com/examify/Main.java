74354e9629cb9ba8dc1f0b6565a55cf5d38c3bad
Berke Ikikarakayali <berke.karakaya35@outlook.com>
2025-12-18 16:18:17 +0300

    Schedule Generation Test with Large Input
    Updated Main.java to test to generate schedule with 500 students and 15 courses. Verified algorithm: The scheduler successfully manages student conflicts and uses multiple classrooms in parallel slots ( 5 different exams scheduled simultaneously in a same slot). Confirmed capacity constraints ( 49 student course to 50 capacity classroom).

--- src/main/java/com/examify/Main.java
+++ src/main/java/com/examify/Main.java
@@ -1,110 +1,77 @@
-package com.examify;
+package com.examify.main;
 
+import com.examify.model.ExamScheduler;
 import com.examify.model.FileImportService;
 import com.examify.model.ScheduleManager;
 import com.examify.model.entities.Classroom;
 import com.examify.model.entities.Course;
 import com.examify.model.entities.Exam;
 import com.examify.model.entities.Student;
-import com.examify.model.ExamScheduler;
-
-//dao imports will be added
 
 import java.util.List;
 
 public class Main {
+
     public static void main(String[] args) {
-        //test scheduling
+        //input files
+        String classroomFile = "data_classrooms.json";
+        String studentFile = "data_students.json";
+        String courseFile = "data_courses.json";
+        String enrollmentFile = "data_enrollments.json";
+
+        FileImportService fileService = new FileImportService();
         ScheduleManager manager = ScheduleManager.getInstance();
-        //test file import
-        FileImportService importService = new FileImportService();
 
-//        System.out.println("--- JSON IMPORT TEST ---");
-//
-//        try {
-//            List<Student> students = importService.importStudentsJson("data_students.json");
-//            List<Classroom> classrooms = importService.importClassroomsJson("data_classrooms.json");
-//            List<Course> courses = importService.importCoursesJson("data_courses.json");
-//            importService.importEnrollmentsJson("data_enrollments.json", courses);
-//            System.out.println("\n--- FINAL DATA CHECK ---");
-//            for (Course c : courses) {
-//                System.out.println("Course: " + c.getCode() + " | Number of Enrolled Students: " + c.getEnrolledStudents().size());
-//            }
-//        } catch (Exception e) {
-//            System.err.println(e.getMessage());
-//        }
-
+        // Clear data
         manager.clearAllData();
 
-        try {
-            List<Student> students = importService.importStudentsCSVFile("sampleData_AllStudents.csv");
-            for (Student s : students) {
-                manager.addStudent(s);
-            }
-            System.out.println("Students loaded: " + manager.getStudents().size());
+        System.out.println("IMPORT JSON FILES");
 
-            List<Classroom> classrooms = importService.importClassroomsCSVFile("sampleData_AllClassroomsAndTheirCapacities.csv");
-            for (Classroom c : classrooms) {
-                manager.addClassroom(c);
-            }
+        List<Classroom> classrooms = fileService.importClassroomsJson(classroomFile);
+        if (classrooms != null) {
+            for (Classroom c : classrooms) manager.addClassroom(c);
             System.out.println("Classrooms loaded: " + manager.getClassrooms().size());
-
-            List<Course> courses = importService.importCoursesCSVFile("sampleData_AllCourses.csv");
-
-            importService.importEnrollmentsCSVFile("sampleData_AllAttendanceLists.csv", courses);
-
-            for (Course co : courses) {
-                manager.addCourse(co);
-            }
-            System.out.println("Courses loaded: " + manager.getCourses().size());
-
-            if (!manager.getCourses().isEmpty()) {
-                Course sampleCourse = manager.getCourses().get(0);
-                System.out.println("Example course (" + sampleCourse.getCode() + ") Size: " + sampleCourse.getEnrolledStudents().size());
-            }
-
-        } catch (Exception e) {
-            e.printStackTrace();
-            return;
         }
+        List<Student> students = fileService.importStudentsJson(studentFile);
+        if (students != null) {
+            for (Student s : students) manager.addStudent(s);
+            System.out.println("Students loaded: " + manager.getStudents().size());
+        }
+        List<Course> courses = fileService.importCoursesJson(courseFile);
+        if (courses != null) {
+            for (Course c : courses) manager.addCourse(c);
+            System.out.println("Courses loaded: " + manager.getCourses().size());
+        }
+        fileService.importEnrollmentsJson(enrollmentFile, manager.getCourses());
+        System.out.println("Enrollments processed.\n");
 
 
-        // initiating algorithm
+        //Run the Algorithm ---
+        System.out.println("Generating Schedule");
+
         ExamScheduler scheduler = new ExamScheduler();
-        long startTime = System.currentTimeMillis();
-
         List<Exam> schedule = scheduler.generateSchedule();
 
-        long endTime = System.currentTimeMillis();
-        System.out.println("Algorithm completed. time: " + (endTime - startTime) + "ms");
-
-        // report results
-        System.out.println("\n--- results report ---");
-        System.out.println("total matched exam: " + schedule.size());
-
-        List<String> errors = scheduler.getErrorLog();
-        if (!errors.isEmpty()) {
-            System.out.println("WARNINGS/ERRORS (" + errors.size() + "):");
-            for (String err : errors) {
+        // --- 3. Display Results ---
+        if (schedule.isEmpty()) {
+            System.err.println("ERROR: Could not generate a schedule.");
+            System.err.println("Error Logs:");
+            for(String err : scheduler.getErrorLog()) {
                 System.out.println(" - " + err);
             }
         } else {
-            System.out.println("There are no conflicts or missing courses.");
+            System.out.println("Schedule generated.\n");
+            printSchedule(schedule);
         }
+    }
 
-        // searching test
-        System.out.println("\n--- searching test (CourseCode_01) ---");
-        if (!schedule.isEmpty()) {
-            String searchKey = "CourseCode_01";
-            List<Exam> results = manager.searchSchedule(searchKey, "Course");
-            System.out.println("Result: " + results.size() + " match found.");
-            for (Exam e : results) {
-                System.out.println("   -> " + e);
-            }
-        } else {
-            System.out.println("The schedule was empty, so the search test could not be performed.");
+    // print the schedule
+    private static void printSchedule(List<Exam> exams) {
+        String format = "%-15s %-10s %-7s %-8s %-10s %n";
+        System.out.format("Course Code - Classroom - Day  -  Slot - Students  %n");
+
+        for (Exam e : exams) {
+            System.out.format(format, e.getCourse().getCode(), e.getClassroom().getId(), e.getDay(), e.getTimeSlot(), e.getCourse().getEnrolledStudents().size());
         }
-
-        System.out.println("\n=== TEST COMPLETED SUCCESSFULLY ===");
     }
 }
